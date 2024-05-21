3ac508f4cd487d0ab41a93f96de16c996496d33f
AyaTarek95 <aya.tarek213@icloud.com>
2024-05-21 18:21:22 +0300

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,3 +22,18 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """def decode_base64_authorization_header method"""
+        if not base64_authorization_header or \
+                not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded.decode('utf-8')
+            return decoded_str
+
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None

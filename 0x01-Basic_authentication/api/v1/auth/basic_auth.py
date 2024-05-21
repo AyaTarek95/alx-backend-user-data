de2d542ac25dec4f311b1ddab77e899045816d31
AyaTarek95 <aya.tarek213@icloud.com>
2024-05-21 17:36:23 +0300

    task6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""BaseicAuth class module"""
+
+
+class BasicAuth(Auth):
+    """BasicAuth class inherits from Auth class"""
+    def __init__(self) -> None:
+        """contructor method"""
+        pass
+

98ea1df05e16115d74cf18476d6229a98bc1828d
AyaTarek95 <aya.tarek213@icloud.com>
2024-05-21 18:06:57 +0300

    task7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """BaseicAuth class module"""
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -8,3 +9,16 @@ class BasicAuth(Auth):
     def __init__(self) -> None:
         """contructor method"""
         pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extract_base64_authorization_header method"""
+
+        if not authorization_header or \
+                not isinstance(authorization_header, str):
+            return None
+
+        if authorization_header[:6] != 'Basic ':
+            return None
+
+        return authorization_header[6:]
